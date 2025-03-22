94c9755476f0d072981bfe3682198ee14251e0ea
AlvaroRuiz1126 <cheruiz.1126@gmail.com>
2025-03-22 00:53:27 -0500

    feat: add front app to display cookie text and lucky number

--- react/ClientCookiesFortune.tsx
+++ react/ClientCookiesFortune.tsx
@@ -1,11 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Spinner, Tag } from "vtex.styleguide";
+import { getDocuments } from "./service";
 
-type Props = {
-  name: string
+function ClientCookieForutne() {
+  const [cookie, setCookie] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [luckyNumber, setLuckyNumber] = useState<string | null>(null);
+
+  const handleCookie = async () => {
+    setLoading(true);
+    const cookiesList = await getDocuments();
+    const randomCookie =
+      cookiesList[Math.floor(Math.random() * cookiesList.length)];
+
+    setCookie(randomCookie?.CookieFortune);
+
+    const randomNumber = Math.floor(
+      Math.random() * (99999999 - 10000000 + 1) + 10000000
+    );
+    const numberToStr = randomNumber.toString();
+    const formattedNumber = numberToStr.replace(
+      /(\d{2})(\d{2})(\d{4})/,
+      "$1-$2-$3"
+    );
+
+    setLuckyNumber(formattedNumber);
+    setLoading(false);
+  };
+
+  return (
+    <div className="flex flex-column items-center justify-between">
+      <div>
+        <Button onClick={handleCookie}>Get Cookie</Button>
+      </div>
+
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex items-end">
+          {cookie.trim().length > 0 && (
+            <h3 className="mv2">
+              <Tag bgColor="#134CD8" color="#FFFFFF" size="large">
+                {cookie}
+              </Tag>
+            </h3>
+          )}
+          {luckyNumber && (
+            <h5 className="mv2">
+              <Tag size="large" variation="low">
+                {luckyNumber}
+              </Tag>
+            </h5>
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
 
-function Greeting({ name }: Props) {
-  return <div>Hey, {name}</div>
-}
-
-export default Greeting
+export default ClientCookieForutne;
